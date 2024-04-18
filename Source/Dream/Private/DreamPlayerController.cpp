23664990b102799f088b783c389dc0a3202cb7e2
gwhitfieldjr <gwhitfieldjr@gmail.com>
2024-04-17 21:34:41 -0400

    Recompile project, see logging statement in editor window.

--- Source/Dream/Private/DreamPlayerController.cpp
+++ Source/Dream/Private/DreamPlayerController.cpp
@@ -2,4 +2,15 @@
 
 
 #include "DreamPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+void ADreamPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	// Get the enhances input subsystem.
+	UE_LOG(LogTemp, Warning, TEXT("log1 beginplay"));
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer())) {
+		// Add the mapping context to enable controls.
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+		UE_LOG(LogTemp, Warning, TEXT("Enhanced subsystem added to ULocalPlayer."));
+	}
+}
--- Source/Dream/Public/DreamPlayerController.h
+++ Source/Dream/Public/DreamPlayerController.h
@@ -6,12 +6,18 @@
 #include "GameFramework/PlayerController.h"
 #include "DreamPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+
 UCLASS()
 class DREAM_API ADreamPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+protected:
+	virtual void BeginPlay() override;
 };
